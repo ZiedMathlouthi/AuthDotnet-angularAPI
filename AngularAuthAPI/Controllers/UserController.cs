912cd87138b9f6f4f633fd8d10377ce2130d9b1f
Zied.mathlouthi@esprit.tn <zied.mathlouthi@esprit.tn>
2024-01-07 16:36:36 +0100

    Login ,signUp and password hasher

---
+++ AngularAuthAPI/Controllers/UserController.cs
@@ -0,0 +1,93 @@
+﻿using AngularAuthAPI.Context;
+using AngularAuthAPI.Helpers;
+using AngularAuthAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace AngularAuthAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly AppDbContext _authContext;
+        public UserController(AppDbContext appDbContext)
+        {
+            _authContext = appDbContext;
+        }
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate([FromBody] User userObj)
+        {
+            if (userObj == null) { return BadRequest(); }
+
+            var user = await _authContext.Users.FirstOrDefaultAsync(x => x.Username == userObj.Username );
+
+            if (user == null) { return NotFound(new { Message = "User not found!" }); }
+
+            if(!PasswordHasher.VerifyPassword(userObj.Password, user.Password))
+            {
+                return BadRequest(new {message = "Password is incorrect "});
+            }
+
+            return Ok(new
+            {
+                Message = "Login Success!"
+            });
+        }
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterUser([FromBody] User userObj)
+        {
+            if (userObj == null) { return BadRequest(); }
+
+            // check username
+            if (await CheckUsernameExistAsync(userObj.Username)) { return BadRequest(new { Message = "Username already exists!" }); }
+
+            // check email
+            if (await CheckEmailExistAsync(userObj.Email)) { return BadRequest(new { Message = "This email is already being used!" }); }
+
+            // check password strength
+            var pass = CheckPasswordStrength(userObj.Password);
+
+            if (!string.IsNullOrEmpty(pass)) { return BadRequest(new { Message = pass.ToString() }); }
+
+            userObj.Password = PasswordHasher.HashPassword(userObj.Password);
+            userObj.Role = "User";
+            userObj.Token = "";
+            await _authContext.Users.AddAsync(userObj);
+            await _authContext.SaveChangesAsync();
+            return Ok(new
+            {
+                Message = "User Registred !"
+            });
+          
+        }
+        private async Task<bool> CheckUsernameExistAsync(string username)
+        {
+            return await _authContext.Users.AnyAsync(x => x.Username == username);
+        }
+
+        private async Task<bool> CheckEmailExistAsync(string email)
+        {
+            return await _authContext.Users.AnyAsync(x => x.Email == email);
+        }
+
+        private string CheckPasswordStrength(string password)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            if (password.Length < 8) { sb.Append("Minimum password length should be 8" + Environment.NewLine); }
+
+            if (!(Regex.IsMatch(password, "[a-z]") && Regex.IsMatch(password, "[A-Z]") && Regex.IsMatch(password, "[0-9]")))
+            {
+                sb.Append("Password should be alphanumeric" + Environment.NewLine);
+            }
+
+            if ((!Regex.IsMatch(password, "[<,>,@,!,#,$,%,^,&,*,(,),_,+,\\[,\\],{,},?,:,;,|,',\\,.,/,~,`,-,=]"))) { sb.Append("Password should contain any special character" + Environment.NewLine); }
+
+            return sb.ToString();
+        }
+    }
+}
